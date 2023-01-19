d98b5caae211662a23a56cb6eb553757949a41dc
Formalemon <formalemon@gmail.com>
2023-01-19 18:12:43 +0530

    Image Ordering

--- JS/login.js
+++ JS/login.js
@@ -1,3 +1,21 @@
 document.getElementById("test-button").addEventListener("click", function () {
-  location.href = "../pass_gen.html";
+  if(JSON.stringify(clickedImages) === JSON.stringify(masterPass)){
+    location.href = "../pass_gen.html";
+  }
+  else{
+    clickedImages = [];
+    document.getElementById("clicks").innerHTML = "Wrong Password";
+  }
 });
+
+var masterPass = [1, 5, 9];
+
+var clickedImages = [];
+
+for (let i = 1; i <= 9; i++){
+  document.getElementById(i).addEventListener("click", function(){
+    clickedImages.push(i);
+    document.getElementById("clicks").innerHTML = clickedImages;
+  });
+}
+
