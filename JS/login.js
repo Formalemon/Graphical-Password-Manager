46cd2cd3914869f6123eba38e97bbdf137325454
Formalemon <formalemon@gmail.com>
2023-01-19 18:57:56 +0530

    Image Sequencing and Randomizer

--- JS/login.js
+++ JS/login.js
@@ -5,8 +5,13 @@ document.getElementById("test-button").addEventListener("click", function () {
   else{
     clickedImages = [];
     document.getElementById("clicks").innerHTML = "Wrong Password";
+    randomizePositions();
   }
 });
+
+window.onload = e => {
+  randomizePositions();
+}
 
 var masterPass = [1, 5, 9];
 
@@ -17,5 +22,27 @@ for (let i = 1; i <= 9; i++){
     clickedImages.push(i);
     document.getElementById("clicks").innerHTML = clickedImages;
   });
+}
+
+var imageElements = [
+  document.getElementById("1"),
+  document.getElementById("2"),
+  document.getElementById("3"),
+  document.getElementById("4"),
+  document.getElementById("5"),
+  document.getElementById("6"),
+  document.getElementById("7"),
+  document.getElementById("8"),
+  document.getElementById("9")
+];
+
+function randomizePositions() {
+  // Shuffle the array
+  imageElements.sort(() => Math.random() - 0.5);
+
+  // Update the grid
+  for (let i = 0; i < imageElements.length; i++) {
+    document.getElementsByClassName("column")[i%3].appendChild(imageElements[i]);
+  }
 }
 
